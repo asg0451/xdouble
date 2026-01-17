d133cadfae1eab448f118139706f61fa8c37d9a5
Miles Frankel <miles.frankel@gmail.com>
2026-01-17 10:46:49 +0800

    Initial Commit

---
+++ xdouble/xdoubleApp.swift
@@ -0,0 +1,17 @@
+//
+//  xdoubleApp.swift
+//  xdouble
+//
+//  Created by miles on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct xdoubleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xdoubleTests/xdoubleTests.swift
@@ -0,0 +1,17 @@
+//
+//  xdoubleTests.swift
+//  xdoubleTests
+//
+//  Created by miles on 1/17/26.
+//
+
+import Testing
+@testable import xdouble
+
+struct xdoubleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
